5094e4001a05e7486475b66e19e99649d1926965
ali yaakoub <ali.yaakoub.2021@gmail.com>
2021-10-31 00:38:14 +0200

    adding a posts limit and a button to increase it.

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect }from 'react'
 import axios from 'axios'
 import Post from './Post';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../../fontAwsome'
 
 const Posts = ({userId, notifySuccess, notifyError}) => {
 
     const [posts, setPosts] = useState([]);
+    const [limit, setLimit] = useState(10);
+    const [max, setMax] = useState(0);
+    const limitInc = 10;
 
     useEffect(() =>{
         const fetch = async () =>{
-            const results = await axios(`${process.env.REACT_APP_API}/posts`)
-            setPosts(results.data);
+            try{
+                const results = await axios(`${process.env.REACT_APP_API}/posts?page=1&limit=${limit}`)
+                setPosts(results.data.filteredPosts);
+                setMax(results.data.max)
+                console.log(results);
+            }
+            catch(err){
+                console.error(err);
+            }
         }
         fetch();
-    },[])
+    },[limit])
 
     return (
-        <div className="text-black posts flex flex-col mx-auto mt-10">
+        <div className="text-black posts flex flex-col mx-auto mt-10 mb-10">
             {posts.map(post => (
                 <Post 
                     userId={userId}
@@ -24,6 +36,11 @@ const Posts = ({userId, notifySuccess, notifyError}) => {
                     notifyError={(msg)=>notifyError(msg)}
                 />
             ))}
+            {posts.length === max ? 
+                null
+            :
+                <FontAwesomeIcon icon={['far','plus-square']} size="4x" className="mx-auto text-blue-400 my-10 cursor-pointer" onClick={()=>setLimit(limit+limitInc)}/>
+            }
         </div>
     )
 }
