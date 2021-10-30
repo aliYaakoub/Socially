620220e326fb67c30465eb2e506b29d7f5160c9e
ali yaakoub <ali.yaakoub.2021@gmail.com>
2021-10-31 00:36:28 +0200

    added an avatars to the users .

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -3,10 +3,11 @@ import axios from 'axios'
 import emptyHeart from '../../img/empty-h.png';
 import fullHeart from '../../img/full-h.png';
 
-const Post = ({data, userId, notifyError, notifySuccess, background}) => {
+const Post = ({data, userId, notifyError, notifySuccess, poster=false}) => {
 
     const [liked, setLiked] = useState(false);
     const [likes, setLikes] = useState(data.likes);
+    const [avatar, setAvatar] = useState('');
 
     async function onLiked(){
         
@@ -15,28 +16,74 @@ const Post = ({data, userId, notifyError, notifySuccess, background}) => {
     }
 
     useEffect(()=>{
+        setLikes(data.likes);
+        if(data.userWhoLikedThis.find(item => item === userId)){
+            setLiked(true);
+        }
+    },[data,userId]);
+
+    useEffect(()=>{
         const fetchLikes = async () => {
-            const results = await axios.get(`${process.env.REACT_APP_API}/posts?postid=${data._id}`)
-            setLikes(results.data[0].likes)
-            if(results.data[0].userWhoLikedThis.find(item => item === userId)){
-                setLiked(true);
-            }
+            const results = await axios.get(`${process.env.REACT_APP_API}/posts/getlikes?postid=${data._id}`)
+            setLikes(results.data.likes);
         }
         fetchLikes();
-    },[liked,data,userId]);
+    },[data._id, liked])
+
+    useEffect(()=>{
+        const fetch = async () => {
+            const results = await axios.get(`https://avatars.dicebear.com/api/adventurer-neutral/${data.username}.svg`);
+            setAvatar(results.data);
+        }
+        fetch();
+    },[data]);
+
+    async function deletePost(){
+        try{
+            const result = await axios.delete(`${process.env.REACT_APP_API}/posts/${data._id}`);
+            console.log(result);
+            if(result.data.message === 'success'){
+                notifySuccess('post deleted successfully')
+            }
+            else{
+                notifyError(result.data.message);
+            }
+        }
+        catch(err){
+            console.error(err);
+            notifyError('post already deleted');
+        }
+    }
 
     return (
-        <div className={background==='white' ? 'relative post my-5 rounded-xl flex flex-col bg-white overflow-hidden shadow':`relative post my-5 rounded-xl flex flex-col  overflow-hidden shadow`}>
-            <div className='p-5 mr-28'>
-                <h1 className="text-3xl pb-5 text-blue-400">{data.username}</h1>
-                <p className="text-xl text-justify">{data.content}</p>
-            </div>
-            <div className="absolute h-full right-0 w-16 sm:w-24 flex flex-col justify-center items-center mx-5 border-l-2 border-blue-300">
-                <div className="flex flex-row items-center">
-                    <img src={liked ? fullHeart : emptyHeart} alt="" onClick={()=>onLiked()} className="cursor-pointer"/>
-                    <p className="text-xl pl-2" >{likes}</p>
+        <div className='flex flex-col my-5'>
+            <div className='relative post rounded-xl flex flex-col bg-white overflow-hidden shadow'>
+                <div className='p-5 mr-28'>
+                    <div className='flex flex-row items-center pb-5'>
+                        <div className=" border-2 border-blue-400 w-10 rounded-xl overflow-hidden h-10 bg-white">
+                            <div dangerouslySetInnerHTML={{ __html: avatar }} />
+                        </div>
+                        <h1 className="text-3xl mx-3 text-blue-400">{data.username}</h1>
+                    </div>
+                    <p className="text-xl text-justify">{data.content}</p>
+                </div>
+                <div className="absolute h-full right-0 w-16 sm:w-24 flex flex-col justify-center items-center mx-5 border-l-2 border-blue-300">
+                    <div className="flex flex-row items-center">
+                        <img src={liked ? fullHeart : emptyHeart} alt="" onClick={()=>onLiked()} className="cursor-pointer"/>
+                        <p className="text-xl pl-2" >{likes}</p>
+                    </div>
                 </div>
             </div>
+            {poster ? 
+                <button 
+                    onClick={()=>deletePost()}
+                    className="text-red-600 bg-white w-32 rounded-b-xl pb-2 mx-auto hover:scale-105 transition-transform transform"
+                >
+                    Delete This Post
+                </button>
+                :
+                null
+            }
         </div>
     )
 }
--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -7,12 +7,20 @@ const Profile = ({username, userId, notifyError, notifySuccess}) => {
 
     const [posts, setPosts] = useState([]);
     const [postContent, setPostContent] = useState('');
+    const [avatar, setAvatar] = useState('');
 
     useEffect(() => {
         const fetch = async () => {
             const results = await axios.get(`${process.env.REACT_APP_API}/posts/${username}`);
-            console.log(results);
             setPosts(results.data);
+        }
+        fetch();
+    },[username,posts]);
+
+    useEffect(()=>{
+        const fetch = async () => {
+            const results = await axios.get(`https://avatars.dicebear.com/api/adventurer-neutral/${username}.svg`);
+            setAvatar(results.data);
         }
         fetch();
     },[username]);
@@ -44,8 +52,16 @@ const Profile = ({username, userId, notifyError, notifySuccess}) => {
     return (
         <div className='min-h-screen bg-gray-400 '>
             <Link to='/' className='fixed left-5 top-5 text-white text-2xl transition-transform transform hover:scale-110'>&#8920; See All Posts</Link>
-            <div className="flex items-center justify-center h-screen">
-                <div className='bg-white w-10/12 md:8/12 lg:w-6/12 h-96 rounded shadow p-5'>
+            
+
+            <div className="relative flex items-center justify-center h-screen flex-col">
+                <div className='flex flex-row m-10 text-3xl items-center bg-white overflow-hidden rounded-xl shadow'>
+                    <div className="w-20">
+                        <div dangerouslySetInnerHTML={{ __html: avatar }} />
+                    </div>
+                    <h1 className='mx-5'>{username}</h1>
+                </div>
+                <div className='bg-white w-10/12 md:8/12 lg:w-6/12 h-96 rounded shadow p-5 mb-16'>
                     <form className='w-full h-full flex flex-col' onSubmit={onSubmit}>
                         <h1 className='text-center mb-5 text-xl'>Type what ever you desire and post it</h1>
                         <textarea 
@@ -58,18 +74,27 @@ const Profile = ({username, userId, notifyError, notifySuccess}) => {
                         <button className='bg-gray-400 h-16 rounded btn-active text-white w-2/4 mx-auto text-2xl mt-5 '>Post</button>
                     </form>
                 </div>
+                <div className="absolute bottom-2 animation">
+                    <a href="#posts" className="animation text-white text-6xl">&#10095;</a>
+                </div>
             </div>
-            <div className="text-black posts flex flex-col mx-auto pb-10">
-                <h1 className="text-white text-3xl text-center p-5">your posts : </h1>
-                {posts.map(post => (
-                    <Post 
-                        background={'white'}
-                        userId={userId}
-                        data={post} key={post._id} 
-                        notifySuccess={(msg)=>notifySuccess(msg)}  
-                        notifyError={(msg)=>notifyError(msg)}
-                    />
-                ))}
+            <div id="posts">
+                {posts.length === 0 ?
+                    <h1 className="text-white text-3xl text-center p-5">your posts will be shown down here.</h1>
+                    :
+                    <div className="text-black posts flex flex-col mx-auto pb-10 px-5">
+                        <h1 className="text-white text-3xl text-center p-5">your posts : </h1>
+                        {posts.map(post => (
+                            <Post
+                                poster={true}
+                                userId={userId}
+                                data={post} key={post._id}
+                                notifySuccess={(msg)=>notifySuccess(msg)}
+                                notifyError={(msg)=>notifyError(msg)}
+                            />
+                        ))}
+                    </div>
+                }
             </div>
         </div>
     )
