9a70acdc65c48fcf3b502c4c43fb501a141ca7a9
ali yaakoub <ali.yaakoub.2021@gmail.com>
2021-11-02 20:29:44 +0200

    minor .

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,6 +1,8 @@
 import React,{ useState, useEffect } from 'react';
 import Input from '../common/Input'
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../../fontAwsome';
 
 const Login = ({setUserId, setIsAdmin, setUsername, setLoggedIn, notifyError, notifySuccess, setPage}) => {
 
@@ -43,32 +45,44 @@ const Login = ({setUserId, setIsAdmin, setUsername, setLoggedIn, notifyError, no
     },[isCancelled])
 
     return (
-        <div className='container relative backdrop shadow'>
-            {isLoading ? <h1 className='text-2xl'>Loading ...</h1> : <h1 className='text-2xl'>Please login</h1>}
-            <form onSubmit={(e)=>onSubmit(e)}>
-                <Input 
-                    label={email.length>0? '':'Email : '}
-                    name='email' 
-                    type="email" 
-                    value={email} 
-                    required={true}
-                    onChange={(e)=>setEmail(e.target.value)}
-                />
-                <Input 
-                    label={password.length>0? '':'Password : '}
-                    name='password' 
-                    type="password" 
-                    value={password} 
-                    required={true}
-                    onChange={(e)=>setPassword(e.target.value)}
-                />
-                <button 
-                    className="btn text-black" 
-                >
-                    Login
-                </button>
-              <p className="text">Don't have an account? <span onClick={()=>setPage('register')}>Register</span> </p>
-            </form>
+        <div>
+            <div className='container relative backdrop shadow'>
+                {isLoading ? <h1 className='text-2xl'>Loading ...</h1> : <h1 className='text-2xl'>Please login</h1>}
+                <form onSubmit={(e)=>onSubmit(e)}>
+                    <Input 
+                        label={email.length>0? '':'Email : '}
+                        name='email' 
+                        type="email" 
+                        value={email} 
+                        required={true}
+                        onChange={(e)=>setEmail(e.target.value)}
+                    />
+                    <Input 
+                        label={password.length>0? '':'Password : '}
+                        name='password' 
+                        type="password" 
+                        value={password} 
+                        required={true}
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                    <button 
+                        className="btn text-black" 
+                    >
+                        Login
+                    </button>
+                <p className="text">Don't have an account? <span onClick={()=>setPage('register')}>Register</span> </p>
+                </form>
+            </div>
+            <div className="footer flex flex-row ">
+                <div className="w-full mb-2 text-white text-center flex flex-row items-center">
+                    <FontAwesomeIcon icon={['fas','code']} />
+                    <p className="ml-3 font-bold">By Ali Yaakoub</p>
+                </div>
+                <a href="https://github.com/aliYaakoub/Socially" target='_blank' rel="noreferrer"className="text-white flex flex-row items-center">
+                    <FontAwesomeIcon icon={['fab','github']} size="2x" />
+                    <p className="ml-3 font-bold underline">See on Github</p>
+                </a>
+            </div>
         </div>
     )
 }
--- src/fontAwsome.js
+++ src/fontAwsome.js
@@ -1,15 +1,17 @@
 // import the library
 import { library } from '@fortawesome/fontawesome-svg-core';
-// import { library } f  '@fortawesome/free-brands-svg-icons';
+// import { library: library2 } from  '@fortawesome/free-brands-svg-icons';
 
 // import your icons
-// import { faMoneyBill } from '@fortawesome/pro-solid-svg-icons';
-// import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faPlusSquare, faComment } from '@fortawesome/free-regular-svg-icons';
 
 library.add(
     // faHeart2,
     faPlusSquare,
-    faComment
+    faComment,
+    faCode,
+    faGithub
   // more icons go here
 );
